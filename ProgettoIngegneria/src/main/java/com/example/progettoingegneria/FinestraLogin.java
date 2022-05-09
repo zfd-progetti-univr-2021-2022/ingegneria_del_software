9f5b3c693370e1cef92c6947691b2ea8675734f7
FarinaChristian <58805490+FarinaChristian@users.noreply.github.com>
2022-05-09 18:41:51 +0200

    Add files via upload

--- ProgettoIngegneria/src/main/java/com/example/progettoingegneria/FinestraLogin.java
+++ ProgettoIngegneria/src/main/java/com/example/progettoingegneria/FinestraLogin.java
@@ -11,29 +11,40 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class FinestraLogin extends Application{
     public void start(Stage stage) {
         Scene scene;
-        Button accedi;
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("FinestraLogin.fxml"));
         try { scene = new Scene(loader.load()); }
         catch (IOException exception) {throw new RuntimeException(exception);}
 
-        accedi= (Button) loader.getNamespace().get("accedi");
+        //prendo le credenziali
+        TextField nomeUtente=(TextField) loader.getNamespace().get("inputUtente");
+        PasswordField password=(PasswordField) loader.getNamespace().get("inputPassword");
+        Button accedi= (Button) loader.getNamespace().get("accedi");
         accedi.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                //prendo le credenziali
-                TextField nomeUtente=(TextField) loader.getNamespace().get("inputUtente");
-                PasswordField password=(PasswordField) loader.getNamespace().get("inputPassword");
                 //se inserisco le credenziali dell'amministratore apro la finestra per aggiungere i dipendenti
                 if(nomeUtente.getText().equals("mario") && password.getText().equals("secret")){
+                    try {
+                        ManagementSystem ms = ManagementSystem.getInstance();
+                        ms.login(nomeUtente.getText(),password.getText());
+                    }
+                    catch (IOException e){System.out.println(e);}
+                    catch (URISyntaxException e){System.out.println(e);}
                     new FinestraDipendente().start(new Stage());
                     stage.close();
                 }
                 else{
+                    try {
+                        ManagementSystem ms = ManagementSystem.getInstance();
+                        ms.login(nomeUtente.getText(),password.getText());
+                    }
+                    catch (IOException e){System.out.println(e);}
+                    catch (URISyntaxException e){System.out.println(e);}
                     new FinestraRicerca().start(new Stage());
                     stage.close();
                 }
