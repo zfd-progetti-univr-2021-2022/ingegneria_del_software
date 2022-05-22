ac58d367c5484b15f17d7160994072e38b756ab4
Stefano Zenaro <mariortgasd@hotmail.com>
2022-05-22 16:26:32 +0200

    + titolo, migliorato try/catch

--- ProgettoIngegneria/src/main/java/com/example/progettoingegneria/FinestraLogin.java
+++ ProgettoIngegneria/src/main/java/com/example/progettoingegneria/FinestraLogin.java
@@ -16,17 +16,18 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class FinestraLogin extends Application{
-    ManagementSystem ms;
+    private ManagementSystem ms;
     public void start(Stage stage) {
         Scene scene;
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("FinestraLogin.fxml"));
-        try { scene = new Scene(loader.load()); }
-        catch (IOException exception) {throw new RuntimeException(exception);}
-
-        try { ms = ManagementSystem.getInstance();}
-        catch (IOException e){System.out.println(e);}
-        catch (URISyntaxException e){System.out.println(e);}
+        try {
+            scene = new Scene(loader.load());
+            ms = ManagementSystem.getInstance();
+        }
+        catch (IOException | URISyntaxException exception) {
+            throw new RuntimeException("Errore lettura/scrittura file json/fxml: " + exception);
+        }
 
         //prendo le credenziali
         TextField nomeUtente=(TextField) loader.getNamespace().get("inputUtente");
@@ -35,8 +36,7 @@ public class FinestraLogin extends Application{
         accedi.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                ms.login(nomeUtente.getText(),password.getText());
-                if(ms.getLoggedInUser()==null)
+                if(!ms.login(nomeUtente.getText(), password.getText()))
                     JOptionPane.showMessageDialog(null, "NOME UTENTE O PASSWORD NON CORRETTI", "ERRORE", JOptionPane.ERROR_MESSAGE);
                 else{
                     new FinestraRicerca().start(new Stage());
@@ -49,8 +49,7 @@ public class FinestraLogin extends Application{
             @Override
             public void handle(KeyEvent ke) {
                 if (ke.getCode().equals(KeyCode.ENTER)) {
-                    ms.login(nomeUtente.getText(),password.getText());
-                    if(ms.getLoggedInUser()==null)
+                    if(!ms.login(nomeUtente.getText(), password.getText()))
                         JOptionPane.showMessageDialog(null, "NOME UTENTE O PASSWORD NON CORRETTI", "ERRORE", JOptionPane.ERROR_MESSAGE);
                     else{
                         new FinestraRicerca().start(new Stage());
@@ -64,8 +63,7 @@ public class FinestraLogin extends Application{
             @Override
             public void handle(KeyEvent ke) {
                 if (ke.getCode().equals(KeyCode.ENTER)) {
-                    ms.login(nomeUtente.getText(),password.getText());
-                    if(ms.getLoggedInUser()==null)
+                    if(!ms.login(nomeUtente.getText(), password.getText()))
                         JOptionPane.showMessageDialog(null, "NOME UTENTE O PASSWORD NON CORRETTI", "ERRORE", JOptionPane.ERROR_MESSAGE);
                     else{
                         new FinestraRicerca().start(new Stage());
@@ -75,6 +73,7 @@ public class FinestraLogin extends Application{
             }
         });
 
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
